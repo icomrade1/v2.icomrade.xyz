83a496c381e4b55a6c37da6c7d82caf57229337b
icomrade1 <denizhan_2006_34@hotmail.com>
2022-06-23 04:04:59 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+
+<template>
+<head>
+          <!-- discord -->
+        <meta content="icomrade.xyz" property="og:title" />
+        <meta content="I'm giving you a night call to tell you how I feel
+                       I want to drive you through the night, down the hills
+                       I'm gonna tell you something you don't want to hear 
+                       I'm gonna show you where it's dark, but have no fear 
+                       There's something inside you
+                       It's hard to explain
+                       They're talking about you, boy
+                       But you're still the same" property="og:description" />
+        <meta content="https://www.icomrade.xyz" property="og:url">
+        <meta content="https://photos.app.goo.gl/jScDdypZq6ob25Cr8" itemprop="image" property="og:image">
+        <meta name="twitter:card" content="summary_large_image">
+       <meta content="#ff0000" data-react-helmet="true" name="theme-color" />
+</head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="in-middle">
+ <button class="audio">Play</button>
+    <span class="gradient" id="maintext">
+      icomrade.xyz
+    </span>
+        <buttons id="btn">
+  <a href="https://github.com/icomrade1" id="btn" class="gradient" style="margin: 20px;"><i class="fa fa-github"></i></a>
+    <a href="https://ide.geeksforgeeks.org/" id="btn" class="gradient" style="margin:20px"><i class="fa fa-discord"></i></a>
+      <a href="https://ide.geeksforgeeks.org/" id="btn" class="gradient" style="margin:20px"><i class="fa fa-spotify"></i></a> 
+</buttons>
+    <span class="gradient" id="sidetext" style="font-family: sf; margin: 0px;">
+        "i'm the best programmer alive, nobody can beat me" - kanye
+    </span>
+      <audio autoplay silent loop>
+<source src="./assets/media/sigma.mp3" type="audio/mpeg">
+  </audio>
+
+</div>
+</template>
+
+<style>
+
+
+</style>
+
+<script>
+
+    </script>
